7d755b5b92c96d0ff30c3476fcf5f4b93b7d050a
BeckyDania <reabekaking@gmail.com>
2021-04-17 16:17:13 -0400

    generic POST route setup

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -7,4 +7,15 @@ languages.get('/', (req, res) => {
   res.send('Get route is working')
 })
 
+languages.post('/', (req, res) => {
+  LanguagesModel.create(req.body, (error, createdLanguage) => {
+    if (error) {
+      res.status(400).json({ error: error.message })
+    }
+    res.status(200).json(createdLanguage) //  .json() will send proper headers in response so client knows it's json coming back
+  })
+})
+
+
+
 module.exports = languages
