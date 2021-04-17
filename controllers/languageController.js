9f8b2aa8d3d87fdc2eea45838a7fa37e726ac914
BeckyDania <reabekaking@gmail.com>
2021-04-17 19:32:16 -0400

    Get Post Delete Update Languages Setup. Will delete Update and Delete when CRUD is figured out

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -4,7 +4,13 @@ const LanguagesModel = require('../models/languagesModel')
 
 
 languages.get('/', (req, res) => {
-  res.send('Get route is working')
+  //res.send('Get route is working')
+  LanguagesModel.find({}, (error, foundLanguages) => {
+    if (error) {
+      res.status(400).json({ error: error.message })
+    }
+    res.status(200).json(foundLanguages) //  .json() will send proper headers in response so client knows it's json coming back
+	})
 })
 
 languages.post('/', (req, res) => {
@@ -16,6 +22,37 @@ languages.post('/', (req, res) => {
   })
 })
 
+//DELETE ROUTE
+languages.delete('/:id', (req, res) => {
+  LanguagesModel.findByIdAndDelete(req.params.id, (error, deletedLanguage) => {
+    if (error) {
+      res.status(400).json({ error: error.message })
+    }
+    else if (deletedLanguage === null){
+    	res.status(404).json({message: 'Translation Not Found'})
+    }
+    res.status(200).json({message: 'Translation of ' + deletedLanguage.content + ' deleted successfully'}) 
+  })
+})
+
+
+//Update Route
+languages.put('/:id', (req, res) => {
+	LanguagesModel.findByIdAndUpdate(req.params.id, req.body, {new: true},  (error, updatedLanguage) => {
+	  if (error) {
+		res.status(400).json({ error: error.message })
+	  }
+	  else if (updatedLanguage === null){
+		  res.status(404).json({message: 'Holiday Not Found'})
+	  }
+	  else{
+		  res.status(200).json({
+			  message: 'Translation for ' + updatedLanguage.content + ' updated successfully',
+			  data: updatedLanguage
+		  }) 
+	  }
+	})
+  })
 
 
 module.exports = languages
