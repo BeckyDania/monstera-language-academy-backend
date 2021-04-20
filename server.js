186ff8e5cbd9f444fc2d74ea5cbc7ecce2ce8979
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-04-19 20:50:40 -0400

    fix CORS whitelist URL

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express()
 
 const cors = require('cors')
 
-const whitelist = ['http://localhost:3060']
+const whitelist = ['http://localhost:3000']
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
