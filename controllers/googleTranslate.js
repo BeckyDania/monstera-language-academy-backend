12363900ee0814eae492e9e3d47adbcb1e8df136
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-04-23 23:20:30 -0400

    2

--- controllers/googleTranslate.js
+++ controllers/googleTranslate.js
@@ -6,10 +6,10 @@ const express = require('express')
 const translations = express.Router()
 const translateTextModel = require('../models/googleTranslateModel');
 //const { query, json, response, text } = require('express');
- 
-const TOKEN_ARG = 2;
-const tokenPath = process.argv[TOKEN_ARG];
-process.env.GOOGLE_APPLICATION_CREDENTIALS = './token.json'
+
+// const TOKEN_ARG = 2;
+// const tokenPath = process.argv[TOKEN_ARG];
+// process.env.GOOGLE_APPLICATION_CREDENTIALS = './token.json'
 
 // Creates a client
 const translate = new Translate();
@@ -23,7 +23,7 @@ translations.post('/', async (req, res) =>{
       /*    {
             "_id": 1, text:1, target:1, detectedSourceLanguage: 1, translation: 1
         }   */
-   
+
      )
     if(cachedTranslation){
         console.log("database hit")
@@ -33,7 +33,7 @@ translations.post('/', async (req, res) =>{
    const detection = await translate.detect(req.body.text);
    //search for text - mongodb return immediately - cache the results from Google Translate
     const translation = await translate.translate(req.body.text, req.body.target);
-    
+
   //  res.send(translations)
     const newTranslation = {
         text: req.body.text,
@@ -42,24 +42,24 @@ translations.post('/', async (req, res) =>{
         translation: translation[0]
     }
     //res.send(newTranslation)
-    
- 
+
+
     translateTextModel.create(newTranslation, (error, createdTranslation) => {
       if (error) {
         res.status(400).json({ error: error.message })
       }
       res.status(200).json(createdTranslation) //  .json() will send proper headers in response so client knows it's json coming back
-     
+
     })
     }
-  
+
   })
-  
- 
+
+
 module.exports = translations
 
 
-/* 
+/*
 const text = [
     "这是一个非常好的API",
   //  "to jest bardzo dobre API",
@@ -76,9 +76,9 @@ let [detections] = await translate.detect(text);
     detections.forEach((detection) => {
         console.log(detection);
     });
-} 
+}
 
-//detectLanguage();  
+//detectLanguage();
 
 const translateText = async function(req, res){
 //async function translateText() {
@@ -93,4 +93,3 @@ const translateText = async function(req, res){
 
 
 //translateText();
-
